1a9c6d95e350b849655ca13e9008c4be75201a22
Francisco Trindade <frank.trindade@gmail.com>
2014-10-15 14:12:07 +1100

    not creating an order for every add to cart button. Trying to avoid race conditions

---
+++ app/controllers/home_controller_decorator.rb
@@ -0,0 +1,3 @@
+Spree::HomeController.class_eval do
+  before_filter { |c| c.current_order(create_order_if_necessary: true) }
+end
---
+++ app/controllers/taxons_controller_decorator.rb
@@ -0,0 +1,3 @@
+Spree::TaxonsController.class_eval do
+  before_filter { |c| c.current_order(create_order_if_necessary: true) }
+end
