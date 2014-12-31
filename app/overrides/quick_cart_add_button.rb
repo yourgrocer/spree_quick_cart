3c1f7f06f33598e5294b41ea6b92f38f205de53a
Francisco Trindade <frank.trindade@gmail.com>
2014-12-31 16:24:01 +1100

    moving override so it works well with caching inner product information

--- app/overrides/quick_cart_add_button.rb
+++ app/overrides/quick_cart_add_button.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :name => "quick_cart_add_button",
-                     :insert_after => "#products span.price",
+                     :insert_bottom => "[data-hook='products_list_item']",
                      :partial => "spree/shared/quick_cart_add_button",
                      :disabled => false)
